8c5c405707df929e24f5d22d3c6c1906f8c74c6f
Renij Shrestha <renijshrestha8@gmail.com>
2022-09-06 22:15:31 +1000

    port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 const http = require("http");
 const path = require("path");
 const methodOverride = require("method-override");
@@ -37,4 +38,4 @@ app.post("/newEvent", async (req, res) => {
   res.redirect("/");
 });
 
-http.createServer(app).listen(3000);
+http.createServer(app).listen(port);
